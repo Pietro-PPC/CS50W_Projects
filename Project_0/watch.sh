afca3da0bf5befc617fef917c45b8df4c52ab7fd
ppc19 <ppc19@inf.ufpr.br>
2021-04-14 00:26:28 -0300

    delete watch.sh

--- Project_0/watch.sh
+++
@@ -1,9 +0,0 @@
-# This is a script that iterates through the scss directory
-# and watches every file inside it, directing the result css
-# codes to the css directory. 
-
-for F in $(ls scss)
-do
-    F=$(basename $F .scss)
-    sass --watch scss/${F}.scss:css/${F}.css &
-done
