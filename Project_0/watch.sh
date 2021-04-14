4695fd368936325ad29f0fdd70cd84433c9ef45e
ppc19 <ppc19@inf.ufpr.br>
2021-04-14 00:23:34 -0300

    comment watch.sh

--- Project_0/watch.sh
+++ Project_0/watch.sh
@@ -1,3 +1,7 @@
+# This is a script that iterates through the scss directory
+# and watches every file inside it, directing the result css
+# codes to the css directory. 
+
 for F in $(ls scss)
 do
     F=$(basename $F .scss)
