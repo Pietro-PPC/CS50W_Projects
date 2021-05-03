ba8069ec8d599db66d5a8e4e3499555a2ddef89f
ppc19 <ppc19@inf.ufpr.br>
2021-05-03 13:49:04 -0300

    visiting /wiki/TITLE shows the raw markdown of the TITLE page

--- Project_1/encyclopedia/views.py
+++ Project_1/encyclopedia/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
 from . import util
 
@@ -8,3 +9,9 @@ def index(request):
         "entries": util.list_entries()
     })
 
+def wikiPage(request, page):
+    entry = util.get_entry(page)
+    if entry == None:
+        return HttpResponse(f"Page not found!")
+    else:
+        return HttpResponse(f"{entry}")
