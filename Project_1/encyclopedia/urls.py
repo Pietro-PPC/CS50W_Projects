ed8dcc56ceb14a451481b43de60e0e041c7beb02
ppc19 <ppc19@inf.ufpr.br>
2021-08-20 18:01:50 -0300

    Search redirects to page

--- Project_1/encyclopedia/urls.py
+++ Project_1/encyclopedia/urls.py
@@ -2,7 +2,9 @@ from django.urls import path
 
 from . import views
 
+app_name = "encyclopedia"
 urlpatterns = [
     path("", views.index, name="index"),
     path("wiki/<str:page>", views.wikiPage, name="wikipage"),
+    path("search", views.searchPage, name="searchpage")
 ]
--- Project_1/encyclopedia/views.py
+++ Project_1/encyclopedia/views.py
@@ -1,15 +1,13 @@
 from markdown2 import markdown
-
 from django.shortcuts import render
+from django.urls import reverse
+from django.http import HttpResponseRedirect
 from django.http import HttpResponse
-
+from django import forms
 from . import util
 
-
-def index(request):
-    return render(request, "encyclopedia/index.html", {
-        "entries": util.list_entries()
-    })
+class searchPageForm(forms.Form):
+    page = forms.CharField(label="page")
 
 def wikiPage(request, page):
     content = util.get_entry(page)
@@ -18,6 +16,25 @@ def wikiPage(request, page):
     else:
         content = markdown(content)
         return render(request, "encyclopedia/page.html", {
+            "form": searchPageForm(),
             "page": page,
             "content": content
         })
+
+def searchPage(request):
+    if request.method == 'POST':
+        form = searchPageForm(request.POST)
+        
+        if form.is_valid():
+            query_page = form.cleaned_data["page"]
+            page = util.get_entry(query_page)
+            if page != None:
+                return HttpResponseRedirect(reverse("encyclopedia:wikipage", args=[query_page]))
+
+    return HttpResponseRedirect(reverse("encyclopedia:index"))
+
+def index(request):
+    return render(request, "encyclopedia/index.html", {
+        "form": searchPageForm(),
+        "entries": util.list_entries()
+    })
