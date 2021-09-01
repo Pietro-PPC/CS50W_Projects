e0174e127f9f1f3fe6bb243480cbfb20ae9a3eaa
ppc19 <ppc19@inf.ufpr.br>
2021-09-01 16:43:36 -0300

    change directortories names, create models

--- 2_commerce/auctions/models.py
+++ 2_commerce/auctions/models.py
@@ -4,3 +4,18 @@ from django.db import models
 
 class User(AbstractUser):
     pass
+
+class Listing(models.Model):
+    title = models.CharField(max_length=64)
+    description = models.TextField()
+    starting_bid = models.FloatField()
+    image_url = models.URLField(null=True)
+
+class Wishlist(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name="wishlist")
+    listing = models.ForeignKey(Listing, on_delete=models.CASCADE)
+
+class Comment(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    listing = models.ForeignKey(Listing, on_delete=models.CASCADE, related_name="comments")
+    text = models.TextField()
