aad5200851d9363eed379f504158e51d217c1628
ppc19 <ppc19@inf.ufpr.br>
2021-09-19 15:46:29 -0300

    new listing page finished

--- 2_commerce/auctions/page_forms.py
+++ 2_commerce/auctions/page_forms.py
@@ -3,18 +3,23 @@ from django import forms
 class NewListingForm(forms.Form):
     title = forms.CharField(label=False, widget=forms.TextInput(
         attrs={'placeholder': 'Title',
-            'class': 'form-control'
+            'class': 'form-control new-listing'
         })
     )
     description = forms.CharField(label=False, widget=forms.Textarea(
-        attrs={'class': 'form-control'}
+        attrs={'class': 'form-control new-listing',
+            'placeholder': 'Item description '}
     ))
     minimum_bid = forms.FloatField(label=False, widget=forms.NumberInput(
-        attrs={'class': 'form-control', 'step':"0.01"}
+        attrs={'class': 'form-control new-listing', 'step':"0.25",
+            'placeholder': 'Starting Price'}
     ))
     category = forms.CharField(label=False, required=False, widget=forms.TextInput(
-        attrs={'class': 'form-control'}
+        attrs={'class': 'form-control new-listing',
+            'placeholder': 'Category (optional)'}
+
     ))
     url = forms.URLField(label=False, required=False, widget=forms.TextInput(
-        attrs={'class': 'form-control'}
+        attrs={'class': 'form-control new-listing',
+            'placeholder': 'Image URL (optional)'}
     ))
