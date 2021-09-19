86a76172e72ea3199f22f61cdae0d7ad0824869d
ppc19 <ppc19@inf.ufpr.br>
2021-09-19 14:12:37 -0300

    refactor page

--- 2_commerce/auctions/page_forms.py
+++ 2_commerce/auctions/page_forms.py
@@ -3,9 +3,18 @@ from django import forms
 class NewListingForm(forms.Form):
     title = forms.CharField(label=False, widget=forms.TextInput(
         attrs={'placeholder': 'Title',
+            'class': 'form-control'
         })
     )
-    description = forms.CharField(widget=forms.Textarea)
-    minimum_bid = forms.FloatField()
-    category = forms.CharField(required=False)
-    url = forms.URLField(required=False, label="Image URL")
+    description = forms.CharField(label=False, widget=forms.Textarea(
+        attrs={'class': 'form-control'}
+    ))
+    minimum_bid = forms.FloatField(label=False, widget=forms.NumberInput(
+        attrs={'class': 'form-control', 'step':"0.01"}
+    ))
+    category = forms.CharField(label=False, required=False, widget=forms.TextInput(
+        attrs={'class': 'form-control'}
+    ))
+    url = forms.URLField(label=False, required=False, widget=forms.TextInput(
+        attrs={'class': 'form-control'}
+    ))
